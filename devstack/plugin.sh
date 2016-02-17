2393b123124b4e78ad7ac8192c66511318952f61
Qeas <alexey.khodos@nexenta.com>
2016-02-17 17:11:13 +0300

    Update plugin.sh

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -3,7 +3,7 @@
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     cd /home/ubuntu
     git clone -b $NS_MANILA_BRANCH https://github.com/Nexenta/manila.git
-    cp -r manila/manila/volume/drivers/nexenta /opt/stack/manila/manila/volume/drivers
+    cp -r manila/manila/share/drivers/nexenta /opt/stack/manila/manila/share/drivers
     cp manila/manila/exception.py /opt/stack/manila/manila/
     cp manila/manila/opts.py /opt/stack/manila/manila/
 fi
