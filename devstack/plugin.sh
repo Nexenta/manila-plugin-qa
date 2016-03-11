65901b78b021482fb855d5b91d936bc5f70faa29
Qeas <alexey.khodos@nexenta.com>
2016-03-11 22:40:27 +0300

    remove manila folder if it exists before cloning

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -2,6 +2,7 @@
 
 if [[ "$1" == "stack" && "$2" == "install" ]]; then
     cd /home/ubuntu
+    rm -rf manila
     git clone -b $NS_CINDER_BRANCH https://github.com/Nexenta/manila.git
     cp -r manila/manila/share/drivers/nexenta /opt/stack/manila/manila/share/drivers
     cp manila/manila/exception.py /opt/stack/manila/manila/
