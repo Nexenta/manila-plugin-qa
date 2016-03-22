1e2d43d31129afcc939bcfbf8ed76116e09d58ad
Alexey Khodos <alexey.khodos@nexenta.com>
2016-03-22 10:03:01 -0700

    append lines instead of changing files

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -4,6 +4,9 @@ if [[ "$1" == "stack" && "$2" == "install" ]]; then
     cd /home/ubuntu
     git clone -b $NS_CINDER_BRANCH https://github.com/Nexenta/manila.git
     cp -r manila/manila/volume/drivers/nexenta /opt/stack/manila/manila/volume/drivers
-    cp manila/manila/exception.py /opt/stack/manila/manila/
-    cp manila/manila/opts.py /opt/stack/manila/manila/
+    printf '\n\nclass NexentaException(ManilaException):\n    message = _("Exception due to Nexenta failure.")\n' >> /opt/stack/manila/manila/exception.py
+    sed -i '/manila.share.drivers.netapp.options.netapp_provisioning_opts,/a\
+    manila.share.drivers.nexenta.options.NEXENTA_CONNECTION_OPTS,\
+    manila.share.drivers.nexenta.options.NEXENTA_NFS_OPTS,\
+    manila.share.drivers.nexenta.options.NEXENTA_DATASET_OPTS,' /opt/stack/manila/manila/opts.py
 fi
