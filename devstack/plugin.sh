83e2977422af79832e928716e61d9907fd4a0c50
Alexey Khodos <alexey.khodos@nexenta.com>
2016-03-24 12:19:55 -0700

    remove all tempest related stuff

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -9,17 +9,5 @@ if [[ "$1" == "stack" && "$2" == "install" ]]; then
     manila.share.drivers.nexenta.options.NEXENTA_CONNECTION_OPTS,\
     manila.share.drivers.nexenta.options.NEXENTA_NFS_OPTS,\
     manila.share.drivers.nexenta.options.NEXENTA_DATASET_OPTS,' /opt/stack/manila/manila/opts.py
-    sed -i 'identity]/a\
-    alt_tenant_name = alt_demo\
-    alt_password = secretadmin\
-    alt_username = alt_demo\
-    tenant_name = demo\
-    password = nova\
-    username = demo', /opt/stack/tempest/etc/tempest.conf
-    sed -i 'share]/a\
-    multitenancy_enabled = False\
-    enable_protocols = nfs\
-    enable_ip_rules_for_protocols = nfs\
-    capability_storage_protocol = NFS', /opt/stack/tempest/etc/tempest.conf
     cp -r manila/manila/share/drivers/nexenta /opt/stack/manila/manila/share/drivers
 fi
